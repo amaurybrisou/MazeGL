0df914f6f6e68adfe5526b10ca895b5794c0c4e9
Amaury Brisou <amaury.brisou@gmail.com>
2013-05-24 14:58:26 -0400

    new file:   Builders/WorldBuilder.js

---
+++ js/Builders/WorldBuilder.js
@@ -0,0 +1,21 @@
+(function(){
+    var f = function(){
+        if(typeof mmo == "undefined"){
+              console.log("Error : Namespace mmo not Loaded");
+              return false;
+            } else if(typeof mmo.Builders == "undefined"){
+                      console.log("Error : Namespace mmo not Loaded");
+                      return fase;
+            }
+            return true;
+        }
+        
+    if(!f){
+        return;
+    }
+    
+    
+    mmo.Builders.WorldBuilder=function(){
+            
+    }
+})();
